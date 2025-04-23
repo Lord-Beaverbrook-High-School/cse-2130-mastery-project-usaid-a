fe8578d6afa0cb2ed542910abbd13ad63a57c45f
alwolfeCBE <83241214+alwolfeCBE@users.noreply.github.com>
2025-04-22 20:10:17 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //make sure to use the entire filename and path (not just the filename)
     }
 }
