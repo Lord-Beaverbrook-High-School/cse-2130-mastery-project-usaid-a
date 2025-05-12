2210e9d65301cf5d74193f798dbeeca3e8de3ed0
usaid-a <usaida1@educbe.ca>
2025-05-12 09:33:34 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,85 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //make sure to use the entire filename and path (not just the filename)
+    public static void main(String[] args) throws IOException {
+        String read_data = read("C:\\Users\\48258003\\IdeaProjects\\file compressor and decompressor\\src\\data_raw.txt");
+        System.out.println(read_data); //prints read data
+        String compressed_data = compress(read_data); //compresses the read data
+        //System.out.println(compressed_data); //prints compressed data
+        write("compressed_data",compressed_data);
+    }
+    public static String compress(String data){
+        int n = data.length();
+        int count = 1;
+        //StringBuilder is a class that is an object that stores data similarly to a string but each character is mutable unlike a string, and it is better for constant changes efficiency wise
+        StringBuilder CompressedStringBuilder = new StringBuilder();
+        for (int i = 1; i < n; i++) {
+            if (data.charAt(i) == data.charAt(i - 1)) {
+                count++; //counts occurrences of characters
+            } else {
+                CompressedStringBuilder.append(count).append(data.charAt(i - 1));
+                count = 1; // Reset count for the new character
+            }
+        }
+        CompressedStringBuilder.append(count).append(data.charAt(n - 1));
+        return CompressedStringBuilder.toString();
+    }
+    public static void decompress(String str){
+
+    }
+    public static String read(String File_name) {
+        File MyObj = new File(File_name);
+        try {
+            Scanner MyReader = new Scanner(MyObj);
+            while (MyReader.hasNextLine()) {
+                String data = MyReader.nextLine();
+
+                String result = data.replaceAll(" ", "");
+                MyReader.close();
+                return result;
+            }
+            MyReader.close();
+        } catch (Exception e) {
+            System.out.println("file not found");
+            e.printStackTrace();
+            return File_name;
+        }
+        return File_name;
+    }
+
+    public static void write(String FileName,String data) throws IOException {
+        boolean append = true; //if file is already created needed for user to decide if the data will overwrite it or be added
+        File file = new File(FileName);
+        try {
+            if (file.createNewFile()){
+                System.out.println("new file created" + file.getName());
+            }
+            else{
+                System.out.println("file already exists");
+                Scanner myObj = new Scanner(System.in);
+                System.out.println("would you like to add to the file or overwrite it? add/overwrite");
+                String AddOrOverWrite = myObj.nextLine();
+                if (AddOrOverWrite.equals("add")){
+                    append = true;
+                }
+                else if (AddOrOverWrite.equals("overwrite")) {
+                    append = false;
+                }
+                else {
+                    System.out.println("invalid input data will be added to the file");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        FileWriter writer = new FileWriter(FileName,append);//true adds to the file false overwrites it
+        writer.append(data);
+        writer.close();
     }
 }
+//known bugs does not compress very last character
